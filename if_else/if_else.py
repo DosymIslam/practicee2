87c529fc1db290e1a9d68d6dc33e731e732505af
DosymIslam <tiktokigavn@gmail.com>
2026-02-04 19:51:52 +0500

    Add if-else control flow examples

---
+++ if_else/if_else.py
@@ -0,0 +1,6 @@
+norm_temp=36
+your_temp=int(input())
+if your_temp>36 or your_temp<36:
+    print("are you sick?")
+else:
+    print("you are not sick")
---
+++ if_else/if_statement.py
@@ -0,0 +1,4 @@
+islam_age=17
+your_age = int(input())
+if islam_age > your_age:
+    print("go out kid")
